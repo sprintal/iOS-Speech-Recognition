a2177fb026beceea67d1bbad45bceb3dbbd8fa1c
kmen0004 <kmen0004@student.monash.edu>
2019-10-31 23:55:44 +1100

    Initial Commit

---
+++ Speech Recognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speech Recognition
+//
+//  Created by Kang Meng on 31/10/19.
+//  Copyright © 2019 Kang Meng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
